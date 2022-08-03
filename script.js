5044f56163cc0cc2d43e568527931186477d5676
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-03 00:11:06 -0300

    Tarefa 7

--- script.js
+++ script.js
@@ -13,7 +13,45 @@ creatingPixel()
 //6
 
 let firstSelect = document.querySelector('.color');
-let blackSelect = document.createElement('div')
-firstSelect.style.backgroundColor = 'black'
+let blackSelect = document.createElement('div');
 firstSelect.className = 'color selected';
 
+//7
+const palette = document.getElementById('color-palette');
+const selectClass = document.getElementsByClassName('selected');
+
+function classes(event){
+    if (event.target.className === 'color'){
+        selectClass[0].classList.remove('selected');
+        event.target.classList.add('selected')
+    }
+}
+palette.addEventListener('click', classes)
+    
+
+//        
+//if (square2.className === 'color selected'){
+//         square1.className = 'color'
+//         square3.className = 'color'
+//         square4.className = 'color'
+        
+//        }
+//        else if (square3.className === 'color selected'){
+//         square1.className = 'color'
+//         square2.className = 'color'
+//         square4.className = 'color'
+//        }
+       
+//        else if (square4.className === 'color selected'){
+//         square1.className = 'color'
+//         square2.className = 'color'
+//         square3.className = 'color'
+// }
+//         else if (square1.className === 'color selected'){
+//         square2.className = 'color'
+//         square3.className = 'color'
+//         square4.className = 'color'
+//     }
+// }
+//      )
+//}
